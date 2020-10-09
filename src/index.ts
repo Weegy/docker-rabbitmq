f66dae94cef8d2ed3b2611bb0cf249972914b3d5
Marcel Wege <mwege@byte5.de>
2020-10-09 12:49:55 +0200

    Remove extracted Files

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,44 @@
 import { connect, Connection } from "amqplib"
+import { dir } from "console"
+import * as extract from "extract-zip"
+import * as fs from "fs"
+import * as glob from "glob"
+import { resolve } from "path"
+import { IExtractMessage } from "./models/extract-message"
 
-var q = "tasks"
-var qe = "encode"
-
-
+const tasksQueue = "tasks"
+const encodeQueue = "encode"
 
 connect("amqp://rabbitmq")
-.then(function(conn) {
-    return conn.createChannel();
-  }).then(function(ch) {
-    return ch.assertQueue(q).then(function(ok) {
-      return ch.consume(q, function(msg) {
+  .then(function (conn) {
+    return conn.createChannel()
+  })
+  .then(function (ch) {
+    return ch.assertQueue(tasksQueue).then(function (ok) {
+      return ch.consume(tasksQueue, async function (msg) {
         if (msg !== null) {
-          console.error(`DEBUG: ${msg.content.toString()}`);
-          ch.ack(msg);
-          return ch.assertQueue(qe).then(function(oka) {
-            return ch.sendToQueue(qe, Buffer.from('start encoding'));
-          });
+          const fileMessage = JSON.parse(msg.content.toString()) as IExtractMessage
+          console.log(fileMessage)
+          const extractDir = `/app/extract/${fileMessage.packId}`
+          ch.ack(msg)
+          await extract(`/app/zip/${fileMessage.fileName}`, {
+            dir: extractDir,
+          })
+            .then(async () => {
+              await glob(`${extractDir}/**/*.wav`, (err, matches) => {
+                matches.forEach((filename) => console.log(filename))
+              })
+              await glob(`${extractDir}/**/*.mp3`, (err, matches) => {
+                matches.forEach((filename) => console.log(filename))
+              })
+              console.log("MIDI FILES")
+              await glob(`${extractDir}/**/*.mid`, (err, matches) => {
+                matches.forEach((filename) => console.log(filename))
+              })
+            })
+            .catch((error) => console.log(`DEBUG: ${error}`))
         }
-      });
-    });
-  }).catch(console.warn);
+      })
+    })
+  })
+  .catch((error) => console.warn(error))
